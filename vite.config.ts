13435ffd7cd1875247530dbac19bd3882c622320
xionglidong <1105188507@qq.com>
2025-08-19 15:52:29 +0800

    优化

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist"
+    rollupOptions: {
+      external: ["@mastra/core", "@mastra/openai"] // 避免打包
+    }
+  },
+  optimizeDeps: {
+    exclude: ["@mastra/core", "@mastra/openai"] // 让 Vite 不去解析
   }
 });
