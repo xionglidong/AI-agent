e7cc829ce0c88b6a447e9a88f0ae93adf99c33c5
xionglidong <1105188507@qq.com>
2025-08-19 08:41:14 +0800

    重构

--- dist/utils/logger.d.ts
+++
@@ -1,5 +0,0 @@
-import winston from 'winston';
-declare const logger: winston.Logger;
-export declare const requestLogger: (req: any, res: any, next: any) => void;
-export default logger;
-//# sourceMappingURL=logger.d.ts.map
