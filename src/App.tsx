a153bb020f5c82e7a9585e9cf9ba3e5af14d4903
xionglidong <1105188507@qq.com>
2025-08-21 15:41:55 +0800

    weatherAgent

--- src/App.tsx
+++ src/App.tsx
@@ -324,7 +324,7 @@ export default function App() {
     
     try {
       const agent = mastraClient.getAgent(
-        "weather-agent"
+        "weatherAgent"
       );
       // 流式响应
       const streamResponse = await agent.stream({
